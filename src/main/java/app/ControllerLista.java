d69757055ccd95948fe376e7c356616dc1b03ae1
João Roberto <robertoo.jof@gmail.com>
2022-12-05 02:25:03 -0300

    listaSeq com metodo de inserir e remover incompletos

--- src/main/java/app/ControllerLista.java
+++ src/main/java/app/ControllerLista.java
@@ -4,13 +4,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ControllerLista {
+import app.listas.ListaSeq;
 
+public class ControllerLista {
+    // lista
+    ListaSeq listaSeq = new ListaSeq(15);
+
+    // mudanças de tela
     @FXML
     private Button btnList;
 
@@ -55,6 +63,7 @@ public class ControllerLista {
         stage.setTitle("Estrutura de dados - Pilha");
         stage.setScene(scene);
     }
+
     @FXML
     void GoToFila(MouseEvent event) throws IOException {
         Stage stage = (Stage) btnQueue.getScene().getWindow();
@@ -81,6 +90,167 @@ public class ControllerLista {
         stage.setTitle("Estrutura de dados - Home");
         stage.setScene(scene);
     }
+    // "controller propriamente dito"
 
+    // inputs de texto
+    @FXML
+    private TextField inputElement;
+    @FXML
+    private TextField inputPosition;
+    @FXML
+    private TextField inputSearch;
+    @FXML
+    private TextField posSearchResult;
 
+    // botões
+    @FXML
+    private Button btnInsert;
+    @FXML
+    private Button btnRemove;
+
+    // slots de texto com conteúdo
+    @FXML
+    private Text t00;
+    @FXML
+    private Rectangle slot00;
+
+    @FXML
+    private Text t01;
+    @FXML
+    private Rectangle slot01;
+
+    @FXML
+    private Text t02;
+    @FXML
+    private Rectangle slot02;
+
+    @FXML
+    private Text t03;
+    @FXML
+    private Rectangle slot03;
+
+    @FXML
+    private Text t04;
+    @FXML
+    private Rectangle slot04;
+
+    @FXML
+    private Text t05;
+    @FXML
+    private Rectangle slot05;
+
+    @FXML
+    private Text t06;
+    @FXML
+    private Rectangle slot06;
+
+    @FXML
+    private Text t07;
+    @FXML
+    private Rectangle slot07;
+
+    @FXML
+    private Text t08;
+    @FXML
+    private Rectangle slot08;
+
+    @FXML
+    private Text t09;
+    @FXML
+    private Rectangle slot09;
+
+    @FXML
+    private Text t10;
+    @FXML
+    private Rectangle slot10;
+
+    @FXML
+    private Text t11;
+    @FXML
+    private Rectangle slot11;
+
+    @FXML
+    private Text t12;
+    @FXML
+    private Rectangle slot12;
+
+    @FXML
+    private Text t13;
+    @FXML
+    private Rectangle slot13;
+
+    @FXML
+    private Text t14;
+    @FXML
+    private Rectangle slot14;
+
+    @FXML
+    private void insert() {
+        System.out.println("Inserindo");
+
+        t00.setVisible(true);
+        slot00.setVisible(true);
+        t00.setText("teste");
+
+        t01.setVisible(true);
+        slot01.setVisible(true);
+        t01.setText("teste");
+
+        t02.setVisible(true);
+        slot02.setVisible(true);
+        t02.setText("teste");
+
+        t03.setVisible(true);
+        slot03.setVisible(true);
+        t03.setText("teste");
+
+        t04.setVisible(true);
+        slot04.setVisible(true);
+        t04.setText("teste");
+
+        t05.setVisible(true);
+        slot05.setVisible(true);
+        t05.setText("teste");
+
+        t06.setVisible(true);
+        slot06.setVisible(true);
+        t06.setText("teste");
+
+        t07.setVisible(true);
+        slot07.setVisible(true);
+        t07.setText("teste");
+
+        t08.setVisible(true);
+        slot08.setVisible(true);
+        t08.setText("teste");
+
+        t09.setVisible(true);
+        slot09.setVisible(true);
+        t09.setText("teste");
+
+        t10.setVisible(true);
+        slot10.setVisible(true);
+        t10.setText("teste");
+
+        t11.setVisible(true);
+        slot11.setVisible(true);
+        t11.setText("teste");
+
+        t12.setVisible(true);
+        slot12.setVisible(true);
+        t12.setText("teste");
+
+        t13.setVisible(true);
+        slot13.setVisible(true);
+        t13.setText("teste");
+
+        t14.setVisible(true);
+        slot14.setVisible(true);
+        t14.setText("teste");
+    }
+
+    @FXML
+    private void remove() {
+        System.out.println("Removendo");
+    }
 }
---
+++ src/main/java/app/listas/ListaSeq.java
@@ -0,0 +1,153 @@
+package app.listas;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ListaSeq {
+    private String dados[]; // Vetor que contém os dados da lista
+    private int nElementos;
+
+    public ListaSeq() {
+        nElementos = 0;
+        dados = new String[100];
+    }
+
+    public ListaSeq(int tamMax) {
+        nElementos = 0;
+        dados = new String[tamMax];
+    }
+
+    /** Verifica se a Lista está vazia */
+    public boolean vazia() {
+        if (nElementos == 0)
+            return true;
+        else
+            return false;
+    }
+
+    /** Verifica se a Lista está cheia */
+    public boolean cheia() {
+        if (nElementos == dados.length)
+            return true;
+        else
+            return false;
+    }
+
+    /** Obtém o tamanho da Lista */
+    public int tamanho() {
+        return nElementos;
+    }
+
+    /**
+     * Obtém o i-ésimo elemento de uma lista.
+     * Retorna -1 se a posição for inválida.
+     */
+    public String elemento(int pos) {
+
+        /*
+         * Se posição estiver fora dos limites <= 0
+         * ou > tamanho da lista
+         */
+        if ((pos > nElementos) || (pos <= 0))
+            return "-1";
+
+        return dados[pos - 1];
+    }
+
+    /**
+     * Retorna a posição de um elemento pesquisado.
+     * Retorna -1 caso não seja encontrado
+     */
+    public int posicao(String valor) {
+        /*
+         * Procura elemento a elemento, se o dado está na
+         * lista. Se estiver, retorna a sua posição no array+1
+         */
+        for (int i = 0; i < nElementos; i++) {
+            if (dados[i].equals(valor)) {
+                return (i + 1);
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Retorna a posição de um elemento pesquisado.
+     * Retorna -1 caso não seja encontrado
+     */
+    public int posicao(String valor, int desloc) {
+        /*
+         * Procura elemento a elemento, se o dado está na
+         * lista. Se estiver, retorna a sua posição no array+1
+         */
+        for (int i = desloc; i < nElementos; i++) {
+            if (dados[i].equals(valor)) {
+                return (i + 1);
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Insere um elemento em uma determinada posição
+     * Retorna false se a lista estiver cheia ou
+     * a posição for inválida. Caso contrário retorna true
+     */
+    public boolean insere(int pos, String valor) {
+        /*
+         * Verifica se a lista está cheia ou se a posicao a ser
+         * inserida eh invalida (i.e., > tamanho da lista+1
+         */
+        if (cheia() || (pos > nElementos + 1) || (pos <= 0)) {
+            return false;
+        }
+
+        /*
+         * Desloca os elementos após pos, uma posicao a
+         * direita. Isso serve para abrir espaço para insercao
+         * do novo elemento
+         */
+        for (int i = nElementos; i >= pos; i--) {
+            dados[i] = dados[i - 1];
+        }
+
+        /* Insere o dado na posicao correta */
+        dados[pos - 1] = valor;
+
+        /* Incrementa o numero de elementos na lista */
+        nElementos++;
+        return true;
+    }
+
+    /**
+     * Remove um elemento de uma determinada posição
+     * Retorna o valor do elemento removido. -1 caso a remoção falhe
+     */
+    public String remove(int pos) {
+        /* Verifica se a posicao eh valida */
+        if ((pos > nElementos) || (pos < 1)) {
+            return "-1";
+        }
+
+        /* Armazena o dado a ser removido na var "dado" */
+        String aux = dados[pos - 1];
+
+        /*
+         * Desloca todos os elementos após 'pos', uma
+         * posicao a esquerda
+         */
+        for (int i = pos - 1; i < nElementos - 1; i++) {
+            dados[i] = dados[i + 1];
+        }
+
+        /* Decrementa o numero de elementos na lista */
+        nElementos--;
+        return aux;
+    }
+
+    public String printArr() {
+        String str = Arrays.toString(dados);
+        return str;
+    }
+}
